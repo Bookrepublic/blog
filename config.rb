bbb1de601949d460e15c89d7bcc84809a41badad
Matteo Scurati <matteo.scurati@gmail.com>
2015-05-26 16:35:27 +0200

    Better header

--- config.rb
+++ config.rb
@@ -3,13 +3,13 @@
 ###
 # Time.zone = "UTC"
 activate :blog do |blog|
-  # blog.prefix = "blog"
+  #blog.prefix = "blog"
 
  # blog.permalink = "{year}/{month}/{day}/{title}.html"
  # Matcher for blog source files
- # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = "post/{year}-{month}-{day}-{title}.html"
  # blog.taglink = "tags/{tag}.html"
- # blog.layout = "layout"
+  blog.layout = "post"
  # blog.summary_separator = /(READMORE)/
  # blog.summary_length = 250
  # blog.year_link = "{year}.html"
@@ -17,13 +17,13 @@ activate :blog do |blog|
  # blog.day_link = "{year}/{month}/{day}.html"
  # blog.default_extension = ".markdown"
 
- blog.tag_template = "tag.html"
- blog.calendar_template = "calendar.html"
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
 
  # Enable pagination
- blog.paginate = true
- blog.per_page = 10
- blog.page_link = "page/{num}"
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
 end
 
 page "/feed.xml", layout: false
